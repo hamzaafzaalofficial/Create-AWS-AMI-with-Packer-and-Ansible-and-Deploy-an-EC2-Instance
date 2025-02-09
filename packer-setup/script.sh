fad78ac5581729baf6b8ae748e67c7ac154ff839
Hamza Afzal <hamzaafzal.official@gmail.com>
2025-02-09 12:47:30 +0500

    Create script.sh

---
+++ packer-setup/script.sh
@@ -0,0 +1,14 @@
+# Install prerequisites
+sudo apt-get update
+sudo apt-get install -y gnupg software-properties-common curl
+
+# Download and add the HashiCorp GPG key
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Add the HashiCorp repository
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Update package list and install Packer
+sudo apt-get update
+sudo apt-get install -y packer
+
